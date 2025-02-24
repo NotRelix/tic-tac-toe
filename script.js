e09e3697c749ba277177c3120627c5ea77ef6d61
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-02-24 11:56:17 +0800

    feat: initialize gameboard with player info and 3x3 board

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("test");
+function Gameboard() {
+  const gameboard = [
+    {
+      player: 'You',
+    },
+    {
+      player: 'Opponent',
+    },
+  ];
+
+  const board = [];
+  const row = 3;
+  const col = 3;
+
+  for (let x = 0; x < col; x++) {
+    const rowCells = [];
+    for (let y = 0; y < row; y++) {
+      rowCells.push(`${x} ${y}`);
+    }
+    board.push(rowCells);
+  }
+
+  return {
+    gameboard,
+    board,
+  }
+}
+
+const gameboard = Gameboard();
+console.log(gameboard);
