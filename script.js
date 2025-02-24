745553e7db0a1d83def70e3f69c780c1c822643a
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-02-24 22:38:43 +0800

    feat: add checkWinner for all winning directions (rows, cols, diagonals)

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ function Cell() {
   let value = 0;    // 0: Empty, 1: You, 2: Opponent
 
   const getValue = () => value;
+
   const setValue = (playerCode) => {
     value = playerCode;
   }
@@ -55,7 +56,7 @@ function GameController() {
   const gameboard = Gameboard();
   const playerOne = 'You';
   const playerTwo = 'Opponent';
-  
+
   let turn = 1;     // 1: Player One, 2: Player Two
 
   const playRound = (x, y) => {
@@ -63,7 +64,15 @@ function GameController() {
       console.log("Choose another one")
       return;
     }
+
     gameboard.setMarker(x, y, getTurn());
+    const isWinner = checkWinner();
+
+    if (isWinner) {
+      console.log(`Player ${getTurn()} Won the game!`);
+      return;
+    }
+
     setNextTurn();
   }
 
@@ -71,9 +80,59 @@ function GameController() {
 
   const setNextTurn = () => turn = (turn === 1) ? 2 : 1;
 
-  // TODO: Add winner function
   const checkWinner = () => {
+    let isWinner = false;
+    const horizontal = horizontalCheck();
+    const vertical = verticalCheck();
+    const diagonal = diagonalCheck();
 
+    console.log(horizontal, vertical, diagonal);
+    
+    if (horizontal === 3 || vertical === 3 || diagonal === 3) {
+      isWinner = true;
+    }
+
+    return isWinner;
+  }
+
+  const horizontalCheck = () => {
+    let mostConsecutive = 0;
+    for (let x = 0; x < 3; x++) {
+      let numberCount = 0;
+      for (let y = 0; y < 3; y++) {
+        if (gameboard.getMarker(x, y) === getTurn()) {
+          numberCount++;
+        }
+      }
+      mostConsecutive = Math.max(mostConsecutive, numberCount);
+    }
+    return mostConsecutive;
+  }
+
+  const verticalCheck = () => {
+    let mostConsecutive = 0;
+    for (let x = 0; x < 3; x++) {
+      let numberCount = 0;
+      for (let y = 0; y < 3; y++) {
+        if (gameboard.getMarker(y, x) === getTurn()) {
+          numberCount++;
+        }
+      }
+      mostConsecutive = Math.max(mostConsecutive, numberCount);
+    }
+    return mostConsecutive;
+  }
+
+  const diagonalCheck = () => {
+    if (gameboard.getMarker(0, 0) === getTurn() &&
+      gameboard.getMarker(1, 1) === getTurn() &&
+      gameboard.getMarker(2, 2) === getTurn() ||
+      gameboard.getMarker(0, 2) === getTurn() &&
+      gameboard.getMarker(1, 1) == getTurn() &&
+      gameboard.getMarker(2, 0) == getTurn()) {
+      return 3;
+    }
+    return 0;
   }
 
   return {
@@ -85,9 +144,4 @@ function GameController() {
 }
 
 const game = GameController();
-game.playRound(0, 0);
-game.playRound(1, 1);
-game.playRound(2, 0);
-game.playRound(2, 0);
-
 game.gameboard.printBoard();
