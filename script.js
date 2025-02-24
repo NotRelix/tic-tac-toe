24b64c6343a87b8e00c2cc2f4f8b609e3efad762
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-02-24 18:16:54 +0800

    feat: implement encapsulation for each Cell object

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function Gameboard() {
+  const board = [];
+  const rowCount = 3;
+  const colCount = 3;
+
   const gameboard = [
     {
       player: 'You',
@@ -8,23 +12,37 @@ function Gameboard() {
     },
   ];
 
-  const board = [];
-  const row = 3;
-  const col = 3;
-
-  for (let x = 0; x < col; x++) {
+  for (let x = 0; x < colCount; x++) {
     const rowCells = [];
-    for (let y = 0; y < row; y++) {
-      rowCells.push(`${x} ${y}`);
+    for (let y = 0; y < rowCount; y++) {
+      rowCells.push(Cell());
     }
     board.push(rowCells);
   }
 
+  const getBoard = () => board;
+
   return {
     gameboard,
-    board,
+    getBoard,
+  }
+}
+
+function Cell() {
+  let value = 0;   // 0: Empty, 1: You, 2: Opponent
+
+  const getValue = () => value;
+  const setValue = (playerCode) => {
+    value = playerCode;
+  }
+
+  return {
+    getValue,
+    setValue,
   }
 }
 
 const gameboard = Gameboard();
-console.log(gameboard);
+gameboard.getBoard()[1][0].setValue(1);
+console.log(gameboard.getBoard()[1][0].getValue());
+console.log(gameboard.getBoard());
